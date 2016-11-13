5647bba42718880151fc0a12efc22e72e0625d22
DanielRuf <daniel@daniel-ruf.de>
2016-11-13 22:52:52 +0100

    added test for calculateChecksum()

--- test/test.js
+++ test/test.js
@@ -1,11 +1,20 @@
 var assert = require('assert');
-var samsungTv = require('../lib/app');
 
+var samsungTv = require('../lib/app');
 var tv = new samsungTv({port: '/dev/tty.usbserial'});
+
 describe('tv', function() {
   describe('#calculateChecksum([0x08, 0x22, 0x00, 0x00, 0x00, 0x00])', function() {
     it('should return 2a', function() {
       assert.equal('2a', tv.calculateChecksum([0x08, 0x22, 0x00, 0x00, 0x00, 0x00]));
+    });
+  });
+});
+
+describe('tv', function() {
+  describe('#calculateChecksum([0x11, 0xFF, 0x01, 0x01])', function() {
+    it('should return 12', function() {
+      assert.equal('12', tv.calculateChecksum([0x11, 0xFF, 0x01, 0x01]));
     });
   });
 });
